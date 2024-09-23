1cdabf85d40e8158ec56c8c147d85a1e71ca9df6
e-bold <enkhmunkh@att.net>
2024-09-23 17:05:41 -0500

    Completed exercises from 03 through 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const answer = [];
+    
+    array.forEach(item => {
+        if(!args.includes(item)) {
+            answer.push(item)
+        }
+        
+    });
+   
+    return answer;
+    
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+
+    let answer = 0;
+
+    if(!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    
+
+    if ((min < 0  || max < 0)) {
+        return "ERROR";
+    };
+
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    };
+
+
+    while(min <= max) {
+        answer +=min;
+        min++;
+    }
+
+
+    
+    return answer;
 
 };
 
