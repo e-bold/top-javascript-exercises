cacaba6f0efda4f559c538ff3467d390a07d409c
E. Enkhbold <enkhmunkh@att.net>
2024-10-15 11:14:50 -0500

    Completed 09-palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const clean = string
+                .toLowerCase()
+                .split('')
+                .filter(character => alphanumerical.includes(character))
+                .join('');
+
+
+    const reversed = clean.split('').reverse().join('');
+
+    return clean === reversed;
 };
 
 // Do not edit below this line
