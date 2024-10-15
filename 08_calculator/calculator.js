510e986660477a57f47fb10c178178e2ba14a854
E. Enkhbold <enkhmunkh@att.net>
2024-10-15 11:05:31 -0500

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+  return a -b;
+};
+
+const sum = function(arr) {
+  let total = 0;
+  for ( let item of arr) {
+    total += item;
+  }
+return total;
+};
+
+const multiply = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let total = 1;
+  for ( let item of arr) {
+    total *= item;
+  }
+  return total;
+};
+
+const power = function(num, pow) {
+  return Math.pow(num, pow);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for (let i = 1; i<= num; i++) {
+    total *= i;
+  }
+	return total;
 };
 
 // Do not edit below this line
