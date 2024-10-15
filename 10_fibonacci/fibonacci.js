70b6bba3b8c33b7cb570227966888b7aff141756
E. Enkhbold <enkhmunkh@att.net>
2024-10-15 11:43:00 -0500

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if(count < 0) {
+        return "OOPS";
+    };
+
+    const fib = [0, 1];
+    for (let i = 2; i <= count; i++) {
+       fib[i] = fib[i - 1] + fib[i - 2];
+       console.log(fib[i])
+    }
+    return fib[count];
 
 };
+
+console.log(fibonacci(7))
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
